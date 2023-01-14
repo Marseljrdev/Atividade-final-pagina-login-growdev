e25ae6daf6edae57f5140c78f9cc18f23e525969
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-01-14 13:34:19 -0300

    Salvando na lista de recados e deslogando

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,17 +1,11 @@
 
 
-function saveInput() {
+/*  function saveInput() {
 
     let descricao = document.querySelector('#descricao');
     let detalhamento = document.querySelector('#detalhamento');
-
     
     
-    let description = document.querySelector('#description');
-    let detalhes = document.querySelector('#detalhes');
-
-    let count = document.querySelector('#count');
-    let countMais = 1;
     
     let data = JSON.parse(localStorage.getItem('data') || '[]');
     
@@ -19,24 +13,80 @@ function saveInput() {
         descricao: descricao.value,
         detalhamento: detalhamento.value
     })
-
+    
     localStorage.setItem('data', JSON.stringify(data));
-
-    data.forEach((item) => {
-        if(item.descricao && item.detalhamento){
-            description.innerHTML = item.descricao
-            detalhes.innerHTML = item.descricao
-        }
-
-    });
-
-    count.innerHTML = countMais++
+    
+    
     
     
     document.getElementById('descricao').value = "";
-    document.getElementById('detalhamento').value = "";
+    document.getElementById('detalhamento').value = "";  
+    
+    
+    
+} 
+*/
+/* document.addEventListener('DOMContentLoaded', (e) => {
 
-}
+   e.preventDefault();
+   
+   mostrarLinha()
+   
+   
+   
+})
+ */
+let btn = document.getElementById('btn-sub');
+
+btn.addEventListener('click', () => {
+
+    let descricao = document.querySelector('#descricao');
+    let detalhamento = document.querySelector('#detalhamento');
+    
+    
+    
+    let data = JSON.parse(localStorage.getItem('data') || '[]');
+    
+    data.push({
+        descricao: descricao.value,
+        detalhamento: detalhamento.value
+    })
+    
+    localStorage.setItem('data', JSON.stringify(data));
+
+    
+    let mostrar = document.getElementById('mostrarLinha');
+    mostrar.innerHTML = '';
+    
+    let getData = JSON.parse(localStorage.getItem('data'));
+    
+    
+    
+        
+        getData.forEach((valor, index, array) => {
+               mostrar.innerHTML += `
+               <tr>
+                   <td data-label="#" id="count">${index + 1}</td>
+                   <td data-label="Descricao" id="description">${valor.descricao}</td>
+                   <td data-label="Detalhamento" id="detalhes" >${valor.detalhamento}</td>
+                   <td data-label="Acao" >
+                   <button type="submit" id="btn-apagar" onclick="removeItem()">Apagar</button>
+                   <button type="submit" id="btn-edit" >Editar</button>
+                   </td>
+               </tr> 
+               `
+               
+           }); 
+
+    
+    document.getElementById('descricao').value = "";
+    document.getElementById('detalhamento').value = "";  
+    
+})
+
+
+
+
 
 let userLogado = JSON.parse(localStorage.getItem('userLogado'));
 
@@ -44,12 +94,21 @@ let logado = document.querySelector('#h1');
 
 logado.innerHTML = `Lista de recados: ${userLogado.nome}`;
 
+
 if(localStorage.getItem('token') === null){
-    alert('voce precisa estar logado');
+    alert('Você precisar estar logado para acessar essa página');
     window.location.href='../SignIn/login.html';
 }
+if(userLogado.email == '' || userLogado.senha == '') {
+    alert("Você precisar estar logado para acessar essa página")
+    window.location.href = '../SignIn/login.html';
+
+}
+
 
 function sair(){
+    localStorage.removeItem('data');
     localStorage.removeItem('token');
+    localStorage.removeItem('userLogado');
     window.location.href='../SignIn/login.html';
 }
